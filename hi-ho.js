ebd3b0c283b06afd302cbd0324967160ce1489a9
Jake Malachowski <5766239+jakemalachowski@users.noreply.github.com>
2022-03-02 12:31:17 -0800

    Update hi-ho.js

--- hi-ho.js
+++ hi-ho.js
@@ -1,10 +1,9 @@
 const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 10000 })
-
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
+    ws.send(`ho! ${message}`)
   })
-  ws.send('ho! ' + (Math.random() + 1).toString(36).substring(7))
+  ws.send('ho!')
 })
