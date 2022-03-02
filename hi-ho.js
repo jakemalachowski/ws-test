a96ed378be1f370f8e9ed83b9e47a70e7d6d1b17
Jake Malachowski <5766239+jakemalachowski@users.noreply.github.com>
2022-03-02 12:33:49 -0800

    Update hi-ho.js

--- hi-ho.js
+++ hi-ho.js
@@ -1,5 +1,7 @@
 const WebSocket = require('ws')
 
+const wss = new WebSocket.Server({ port: 10000 })
+
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
